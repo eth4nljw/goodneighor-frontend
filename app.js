1e8cbc4ab6587ecbfd69e9c23475beddda478208
JIAWANG LIU <luke570064514@gmail.com>
2022-05-04 12:49:04 +0800

    login

--- app.js
+++ app.js
@@ -1,19 +1,38 @@
 // app.js
 App({
   onLaunch() {
-    // 展示本地存储能力
+      const app = this
+    // // 展示本地存储能力
     const logs = wx.getStorageSync('logs') || []
     logs.unshift(Date.now())
     wx.setStorageSync('logs', logs)
-
     // 登录
     wx.login({
       success: res => {
-        // 发送 res.code 到后台换取 openId, sessionKey, unionId
+        if (res.code) {
+            wx.request({
+              url: `${app.globalData.baseUrl}/login`,
+              method: 'post',
+              data: {
+                code: res.code
+              },
+              success(res) {
+                app.globalData.header = res.data.headers // save JWT to be used when making a request
+                app.globalData.user = res.data.user
+                console.log("login success", app.globalData.user)
+              }
+            })
+          } else {
+            console.log("wx login failed")
+          }
       }
     })
   },
   globalData: {
-    userInfo: null
+    userInfo: null,
+    baseUrl: "http://localhost:3000/api/v1",
+    header: null,
+    user: null
   }
 })
+

b47302f032d7d6875863359e810d9aeb4b8d5559
JIAWANG LIU <luke570064514@gmail.com>
2022-05-08 20:34:34 +0800

    1

--- pages/itemsShow/itemsShow.js
+++ pages/itemsShow/itemsShow.js
@@ -26,8 +26,10 @@ Page({
      * Page initial data
      */
     data: {
-
-    },
+        userInfo: {},
+        hasUserInfo: false,
+        canIUseGetUserProfile: false,
+      },
 
     /**
      * Lifecycle function--Called when page load
@@ -36,6 +38,26 @@ Page({
         const page = this
         page.getItem(options)
     },
+      onLoad() {
+    if (wx.getUserProfile) {
+      this.setData({
+        canIUseGetUserProfile: true
+      })
+    }
+  },
+  getUserProfile(e) {
+    // 推荐使用wx.getUserProfile获取用户信息，开发者每次通过该接口获取用户个人信息均需用户确认
+    // 开发者妥善保管用户快速填写的头像昵称，避免重复弹窗
+    wx.getUserProfile({
+      desc: '用于完善会员资料', // 声明获取用户个人信息后的用途，后续会展示在弹窗中，请谨慎填写
+      success: (res) => {
+        this.setData({
+          userInfo: res.userInfo,
+          hasUserInfo: true
+        })
+      }
+    })
+  },
 
     /**
      * Lifecycle function--Called when page is initially rendered
@@ -64,6 +86,7 @@ Page({
     onUnload: function () {
 
     },
+    
 
     /**
      * Page event handler function--Called when user drop down
